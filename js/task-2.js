41af0512137dd4360adcfb3d39efc5933a36001c
RomanBen <postromanben@gmail.com>
2025-09-05 13:43:54 +0300

    add task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,4 @@
-const getUsersWithFriend = (users, friendName) => 
-
+const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName));
 
 
 const allUsers = [
